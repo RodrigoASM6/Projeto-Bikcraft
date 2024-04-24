5c323d35780617fef7ed0c3f2e056b328540d493
Rodrigo <rodrigomareque8@gmail.com>
2024-04-24 19:27:00 -0300

    introduzindo Javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,55 @@
-console.log("teste");
+// Ativar links do Menu
+
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+    
+    if (url.includes (href)) {
+        link.classList.add("ativo");
+    }
+   
+}
+
+
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementsById(parametro);
+    if(elemento){
+        elemento.checked = true;
+    }
+    
+    console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa');
+    pergunta.setAttribute('aria-expanded', ativa);
+    console.log(controls);
+
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
